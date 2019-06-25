b909f832abb40e120bad622541c62fd3a39e369e
haugstve <daniel.haugstvedt@gmail.com>
2019-06-25 15:04:53 +0200

    Fix tests as instructed in tutorial

--- src/test_example.py
+++ src/test_example.py
@@ -4,20 +4,21 @@ def addition(n1, n2):
     """
     I'm a failing doctest. Please fix me.
     >>> addition(10, 12)
-    20
+    22
     """
-    return n1 - n2
+    return n1 + n2
 
 def subtraction(n1, n2):
     """
     I'm subtraction.
     """
-    return n1 + n2
+    return n1 - n2
 
-class TestExercises(unittest.TestCase):
-    def test_subtraction(self):
-        """
-        I'm a failing unittest. Fix me.
-        """
-        assert subtraction(5, 5) == 0
+ class TestExercises(unittest.TestCase):
+     def test_subtraction(self):
+         """
+         I'm a failing unittest. Fix me.
+         """
+         assert subtraction(5, 5) == 0
 
+         
